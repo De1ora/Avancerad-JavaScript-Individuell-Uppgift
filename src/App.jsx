9b6f9eedbf58fa7d3570655fa6f4bf7eb78e0b58
Lisa <lisafalth05@gmail.com>
2025-09-01 10:20:53 +0200

    Testing around with the Material UI Component library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { BrowserRouter } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
 
 function App() {
   return (
     <BrowserRouter>
       {/* Your app content */}
+      <ButtonUsage />
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
